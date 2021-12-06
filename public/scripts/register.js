3aecca03d3e0da9d81555dc858c7f3a3cbd73b6a
tanselberkant <tanselberkant@gmail.com>
2021-12-07 01:18:28 +0300

    Register html and js is done

---
+++ public/scripts/register.js
@@ -0,0 +1,50 @@
+const myRegisterForm = document.querySelector('.registerForm');
+const firstNameInput = document.querySelector('.firsNameInput');
+const lastNameInput = document.querySelector('.lastNameInput');
+const studentNumberInput = document.querySelector('.studenNoInput');
+const emailInput = document.querySelector('.emailInput');
+const passwordInput = document.querySelector('.passwordInput');
+const formAlert = document.querySelector('.form-alert');
+const formButton = document.querySelector('#myButton');
+
+myRegisterForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+  const studentNumber = studentNumberInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  try {
+    await axios.post('/api/users', {
+      firstName,
+      lastName,
+      studentNumber,
+      email,
+      password,
+    });
+    firstNameInput.value = '';
+    lastNameInput.value = '';
+    studentNumberInput.value = '';
+    emailInput.value = '';
+    passwordInput.value = '';
+    formAlert.style.display = 'block';
+    formAlert.innerHTML = `Kayıt işlemi Başarılı`;
+    formAlert.classList.add('text-success');
+    const changePage = () => {
+      setTimeout(() => {
+        formAlert.style.display = 'none';
+        formAlert.classList.remove('text-success');
+        window.location.href = '../login.html';
+      }, 3000);
+    };
+    changePage();
+  } catch (error) {
+    formAlert.style.display = 'block';
+    formAlert.innerHTML = `Error!, Please Try Again Later`;
+  }
+});
+
+
+
+
